5cd23b82cd7f69ffebe70ebe5d691443054c3bf9
arshadnawazbaig <arshadnawazbaig@gmail.com>
2023-09-20 02:46:02 +0500

    theme fixes

--- context/ThemeProvider.js
+++ context/ThemeProvider.js
@@ -1,20 +1,33 @@
 'use client';
 import { themes } from '@/theme';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 
 const ThemeContext = createContext();
+
+const getFromLocalStorage = () => {
+  if (typeof window !== 'undefined') {
+    const value = localStorage.getItem('theme');
+    return value || 'light';
+  }
+};
 
 export const useTheme = () => {
   return useContext(ThemeContext);
 };
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(() => {
+    return getFromLocalStorage();
+  });
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
+
+  // useEffect(() => {
+  //   localStorage.setItem('theme', theme);
+  // }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
