3f1d1a4f0ce177269df8893b3e93b0d6c18df7a3
arshadnawazbaig <arshadnawazbaig@gmail.com>
2023-09-24 00:27:45 +0500

    related posts fixes

--- components/RelatedPosts/index.js
+++ components/RelatedPosts/index.js
@@ -1,21 +1,37 @@
-'use client';
 import { Box } from '@/style';
 import React from 'react';
-import useSWR from 'swr';
+// import useSWR from 'swr';
 import FeaturePostCard from '../FeaturePostCard';
 import Heading from '../Heading';
 import Divider from '../Divider';
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const getData = async (post) => {
+  const res = await fetch(
+    `${process.env.NEXTAUTH_URL}/api/posts/related?tags=${post?.tags.join(
+      ','
+    )}`,
+    {
+      cache: 'no-store',
+    }
+  );
+
+  if (!res.ok) {
+    console.log('error');
+  }
+
+  return res.json();
+};
 
 const RelatedPosts = async ({ post, targetId }) => {
-  const { data, isLoading } = useSWR(
-    post?.tags ? `/api/posts/related?tags=${post?.tags.join(',')}` : null,
-    fetcher
-  );
+  // const { data, isLoading } = useSWR(
+  //   post?.tags ? `/api/posts/related?tags=${post?.tags.join(',')}` : null,
+  //   fetcher
+  // );
+
+  const posts = await getData(post);
   return (
     <>
-      {data?.length > 0 && (
+      {posts?.length > 0 && (
         <Box className="col-12">
           <Heading
             title="Related posts"
@@ -25,8 +41,8 @@ const RelatedPosts = async ({ post, targetId }) => {
           <Divider />
         </Box>
       )}
-      {data?.length > 0 &&
-        data
+      {posts?.length > 0 &&
+        posts
           ?.filter((post) => post.id !== targetId)
           .map((post) => (
             <Box className="col-sm-6 col-lg-4 col-xxl-3" key={post.id}>
